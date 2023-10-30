5be9c858681cba1da0010e6c18721af244e57535
깃chengsukim <kcs981123@naver.com>
2023-10-30 13:13:23 +0900

    https://github.com/chengsukim/java_text_board_exam.git

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,7 +1,14 @@
+
+
 public class Member {
     long id;
-
     String userId;
     String password;
+    String regDate;
 
+    Member(String userId , String password , String regDate){
+        this.userId = userId;
+        this.password = password;
+        this.regDate = regDate;
+    }
 }
